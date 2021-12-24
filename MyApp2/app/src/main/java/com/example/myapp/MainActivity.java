4b654f8bc801ef757e86749aee642171e8fc2f66
Alia Zeenat <bitf18a522@pucit.edu.pk>
2021-12-24 21:45:39 +0500

    appbar setting

--- MyApp2/app/src/main/java/com/example/myapp/MainActivity.java
+++ MyApp2/app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,12 +14,12 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     TextView txt;
     Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10;
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        toolbar=(Toolbar) findViewById(R.id.myToolBar);
         txt = (TextView) findViewById(R.id.app);
         b1 = (Button) findViewById(R.id.Red);
         b2 = (Button) findViewById(R.id.Black);
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         b8  = (Button) findViewById(R.id.Grey);
         b9= (Button) findViewById(R.id.Brown);
         b10  = (Button) findViewById(R.id.Orange);
+        
         final MediaPlayer mp1= MediaPlayer.create(getApplicationContext(), R.raw.red);
         final MediaPlayer mp2= MediaPlayer.create(getApplicationContext(), R.raw.blue);
         final MediaPlayer mp3= MediaPlayer.create(getApplicationContext(), R.raw.green);
